ee4b5ded528652d001cd1dd6daec8fcd11e91391
DavidEspada14 <102669238+DavidEspada14@users.noreply.github.com>
2022-05-19 15:24:34 +0200

    Add files via upload

---
+++ DBA2 - MongoTemplate/src/test/java/com/espada/DBA2/Dba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espada.DBA2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dba2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
